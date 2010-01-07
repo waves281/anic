b80ba14e11cc5b99e242ccaeee9563530812b25c
kab <kab@localhost>
2010-01-07 00:07:57 -0500

    minor install output tweak

--- bld/installBinary.sh
+++ bld/installBinary.sh
@@ -2,13 +2,16 @@
 
 ### Installer script
 
+echo Running installer...;
+
 if [ -a var/testCertificate.dat ]; then
-	echo Installing...; cp -f $1 $2/$1; echo Done installing.;
+	echo Installing...; cp -f $1 $2/$1; echo Installation complete.;
 else
+	echo
 	echo Warning: Default test cases have not been passed.
 	read -p "Proceed anyway (y/n)? " yn
 	case $yn in
-		[Yy]* ) echo Installing...; cp -f $1 $2/$1; echo Done installing.;;
+		[Yy]* ) echo Installing...; cp -f $1 $2/$1; echo Installation complete.;;
 		[Nn]* ) exit;;
 		* ) echo "Assuming 'no'.";;
 	esac
--- bld/runTests.sh
+++ bld/runTests.sh
@@ -9,11 +9,13 @@ $*
 if [ $? == 0 ]; then
 	echo --------------------------------
 	echo Default test cases passed.
+	echo
 	mkdir -p var
 	echo OK > var/testCertificate.dat
 else
 	echo --------------------------------
 	echo Failed default test cases!
+	echo
 	rm -f var/testCertificate.dat
 fi
 exit 0

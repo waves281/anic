e25f82ab56d8a630b86be549b68e2fee113dc510
kab <kab@localhost>
2010-08-25 22:26:18 -0700

    got rid of program label in error and warning messages

--- src/driver.h
+++ src/driver.h
@@ -16,19 +16,19 @@ void die(void);
 #define print(s) if (!silentMode) { cout << s << "\n"; }
 #define printLabel(s) if (!silentMode) { cout << "\n" << s << "\n"; }
 #define printNotice(s) if (!silentMode) { cout << PROGRAM_STRING << ": " << s << ".\n"; }
-#define printError(s) if (!silentMode) { cerr << PROGRAM_STRING << ": ERROR: " << s << ".\n"; }
-#define printWarning(s) if (!silentMode) { cerr << PROGRAM_STRING << ": warning: " << s << ".\n"; }
+#define printError(s) if (!silentMode) { cerr << "ERROR: " << s << ".\n"; }
+#define printWarning(s) if (!silentMode) { cerr << "WARNING: " << s << ".\n"; }
 
 #define GET_FILE_NAME(fi) ((fi != STANDARD_LIBRARY_FILE_INDEX) ? inFileNames[fi] : STANDARD_LIBRARY_FILE_NAME)
 
-#define lexerError(fi,r,c,str) if (!silentMode) { cerr << PROGRAM_STRING << ": ERROR: LEXER: " << GET_FILE_NAME(fi) << ":" << r << ":" << c << ": " << str << ".\n"; } \
+#define lexerError(fi,r,c,str) if (!silentMode) { cerr << "ERROR: LEXER: " << GET_FILE_NAME(fi) << ":" << r << ":" << c << ": " << str << ".\n"; } \
 	lexerErrorCode++; \
 	if (eventuallyGiveUp && lexerErrorCode >= TOLERABLE_ERROR_LIMIT) { printError("too many errors, giving up"); return NULL; }
 
-#define parserError(fi,r,c,str) if (!silentMode) { cerr << PROGRAM_STRING << ": ERROR: PARSER: " << GET_FILE_NAME(fi) << ":" << r << ":" << c << ": " << str << ".\n"; } \
+#define parserError(fi,r,c,str) if (!silentMode) { cerr << "ERROR: PARSER: " << GET_FILE_NAME(fi) << ":" << r << ":" << c << ": " << str << ".\n"; } \
 	parserErrorCode++;
 
-#define semmerError(fi,r,c,str) if (!silentMode) { cerr << PROGRAM_STRING << ": ERROR: SEMMER: " << GET_FILE_NAME(fi) << ":" << r << ":" << c << ": " << str << ".\n"; } \
+#define semmerError(fi,r,c,str) if (!silentMode) { cerr << "ERROR: SEMMER: " << GET_FILE_NAME(fi) << ":" << r << ":" << c << ": " << str << ".\n"; } \
 	semmerErrorCode++;
 
 #endif
